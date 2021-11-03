89be7edc2a1ce323593e67e7ae3aaa28438c5d56
Mahadevan <rainbow.iad.maha@gmail.com>
2021-11-03 14:22:07 +0800

    Promo card - modified some part 1
    Promo card - modified some part 1

--- src/app/admin/components/user-setting/shop-user-outlet-form/shop-user-outlet-form.component.ts
+++ src/app/admin/components/user-setting/shop-user-outlet-form/shop-user-outlet-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { AppUser } from 'src/app/shared/models/user';
 import { AuthService } from 'src/app/shared/services/auth.service';
 import { ShopUserService } from 'src/app/shop.service';
 
@@ -13,7 +14,7 @@ import { ShopUserService } from 'src/app/shop.service';
 export class ShopUserOutletFormComponent implements OnInit {
 
   error: string;
-  appUser: import("c:/AngularProjects/Promo/PromoSGAdmin/src/app/shared/models/user").AppUser;
+  appUser: AppUser;
   authSubscription: Subscription;
   outletList: any[];
   addressTitle: string = "Add outlet details"
